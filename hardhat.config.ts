27bacacd7fe99fb492ebbcbf4a446eb5b6e1cc50
silverstar <silverstar66703@gmail.com>
2023-11-18 00:31:05 +0900

    update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -14,48 +14,36 @@ dotenv.config();
 // Ensure everything is in place
 let DEPLOYER_MNEMONIC: string;
 if (!process.env.DEPLOYER_MNEMONIC) {
-  	throw new Error('Please set your DEPLOYER_MNEMONIC in the .env file')
+	throw new Error('Please set your DEPLOYER_MNEMONIC in the .env file')
 } else {
-  	DEPLOYER_MNEMONIC = process.env.DEPLOYER_MNEMONIC;
+	DEPLOYER_MNEMONIC = process.env.DEPLOYER_MNEMONIC;
 }
 let ETHERSCAN_API_KEY: string;
 if (!process.env.ETHERSCAN_API_KEY) {
-  throw new Error('Please set your ETHERSCAN_API_KEY in the .env file')
+	throw new Error('Please set your ETHERSCAN_API_KEY in the .env file')
 } else {
 	ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 }
 
-let mainnetForkingSetting = {
-	url: `https://bsc-dataseed.binance.org/`,
-	blockNumber: 30159063 // 31429847
-};
-
-let testnetForkingSetting = {
-	url: "https://data-seed-prebsc-1-s1.binance.org:8545",
-};
-
 const config: HardhatUserConfig = {
-  	solidity: {
+	solidity: {
 		compilers: [
-				{
-					version: '0.8.19',
-					settings: {
-						viaIR: true,
-						optimizer: {
-							enabled: true,
-							runs: 10000
-						}
+			{
+				version: '0.8.19',
+				settings: {
+					viaIR: true,
+					optimizer: {
+						enabled: true,
+						runs: 10000
 					}
-				},
-				{
-					version: '0.4.11'
 				}
-			]
+			},
+			{
+				version: '0.4.11'
+			}
+		]
 	},
 	networks: {
-		hardhat: {
-            forking: mainnetForkingSetting
-        },
 		bsc_testnet: {
 			url: "https://data-seed-prebsc-1-s1.binance.org:8545",
 			chainId: 97,
@@ -70,15 +58,12 @@ const config: HardhatUserConfig = {
 		}
 	},
 	etherscan: {
-			apiKey: ETHERSCAN_API_KEY
-	},
-	mocha: {
-			grep: '^(?!.*; using Ganache).*'
+		apiKey: ETHERSCAN_API_KEY
 	},
 	contractSizer: {
-			alphaSort: true,
-			runOnCompile: true,
-			disambiguatePaths: false,
+		alphaSort: true,
+		runOnCompile: true,
+		disambiguatePaths: false,
 	},
 	gasReporter: {
 		enabled: true
